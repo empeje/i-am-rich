7a5caf08d2f1fc7b2f6ffeb87545b80c85bcf387
empeje <11813607+empeje@users.noreply.github.com>
2022-12-26 23:21:47 +0100

    Initial commit

---
+++ app/src/main/java/io/mpj/minted/MainActivity.java
@@ -0,0 +1,13 @@
+package io.mpj.minted;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
